d08c17aa8a0bdb6a2662377dd7346208d9f3a8ef
Corey Hines <cbh344@outlook.com>
2025-09-03 14:27:20 +1200

    convert print page to be rendered client side

--- src/app/print/page.tsx
+++ src/app/print/page.tsx
@@ -1,28 +1,9 @@
-import OpenPrintDialog from "@/components/open-print-dialog";
-import { deserialisePalletLabel } from "@/utils/serialiser";
-import type { Metadata } from "next";
-import { redirect } from "next/navigation";
+"use client";
 
-export const metadata: Metadata = {
-	title: "Print Pallet Label",
-	description: "Easily create and print pallet labels",
-};
+import PrintPageContent from "@/components/print-page-content";
+import { Suspense } from "react";
 
-export default async function PrintPage({
-	searchParams,
-}: {
-	searchParams: Promise<{
-		[key: string]: string;
-	}>;
-}) {
-	const params = await searchParams;
-	const palletLabel = deserialisePalletLabel(new URLSearchParams(params));
-
-	// Don't generate a pallet label if there aren't any items on it
-	if (palletLabel.items.length == 0) {
-		return redirect("/");
-	}
-
+const PrintPage = () => {
 	return (
 		<>
 			{/* Regular screen view */}
@@ -31,56 +12,11 @@ export default async function PrintPage({
 					id="page"
 					className="bg-white shadow-sm mx-auto w-[210mm] min-h-[297mm]"
 				>
-					<div className="p-8">
-						{/* Pallet title */}
-						{palletLabel.title && (
-							<div className="flex items-center justify-center mb-8">
-								<h1 className="text-7xl font-bold px-4 text-center text-black break-all uppercase">
-									{palletLabel.title}
-								</h1>
-							</div>
-						)}
-
-						{/* Items */}
-						<div className="mt-8 border-y-2 border-y-gray-800">
-							<div className="flex flex-col">
-								{palletLabel.items.map((item, index) => (
-									<p
-										key={index}
-										className="text-center py-3 text-7xl sm:text-8xl text-black font-bold break-all"
-									>
-										{item.sku}{" "}
-										<span className="text-5xl sm:text-6xl lg:text-7xl">
-											x{item.quantity}
-										</span>
-									</p>
-								))}
-							</div>
-						</div>
-
-						{/* Total items count */}
-						<div className="mt-8 text-right">
-							<p className="text-2xl font-medium text-black">
-								Total Items:
-								<span className="font-bold ml-2">
-									{palletLabel.items.reduce(
-										(sum, item) =>
-											sum + Number(item.quantity), // should always be a number
-										0
-									)}
-								</span>
-							</p>
-						</div>
-
-						{/* Footer */}
-						<div className="mt-8 text-center text-sm text-gray-500">
-							https://pallet-labels.coreyh.nz
-						</div>
-					</div>
+					<Suspense>
+						<PrintPageContent />
+					</Suspense>
 				</div>
 			</div>
-
-			<OpenPrintDialog />
 
 			{/* Print styles */}
 			<style>
@@ -116,4 +52,6 @@ export default async function PrintPage({
 			</style>
 		</>
 	);
-}
+};
+
+export default PrintPage;
---
+++ src/components/print-page-content.tsx
@@ -0,0 +1,73 @@
+import { deserialisePalletLabel } from "@/utils/serialiser";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+const PrintPageContent = () => {
+	const searchParams = useSearchParams();
+	const palletLabel = deserialisePalletLabel(
+		new URLSearchParams(searchParams.toString())
+	);
+
+	useEffect(() => {
+		if (palletLabel.items.length === 0) {
+			window.location.href = "/";
+			return;
+		}
+
+		const handle = window.requestAnimationFrame(() => {
+			window.print();
+		});
+
+		return () => window.cancelAnimationFrame(handle);
+	}, [palletLabel]);
+
+	return (
+		<div className="p-8">
+			{/* Pallet title */}
+			{palletLabel.title && (
+				<div className="flex items-center justify-center mb-8">
+					<h1 className="text-7xl font-bold px-4 text-center text-black break-all uppercase">
+						{palletLabel.title}
+					</h1>
+				</div>
+			)}
+
+			{/* Items */}
+			<div className="mt-8 border-y-2 border-y-gray-800">
+				<div className="flex flex-col">
+					{palletLabel.items.map((item, index) => (
+						<p
+							key={index}
+							className="text-center py-3 text-7xl sm:text-8xl text-black font-bold break-all"
+						>
+							{item.sku}{" "}
+							<span className="text-5xl sm:text-6xl lg:text-7xl">
+								x{item.quantity}
+							</span>
+						</p>
+					))}
+				</div>
+			</div>
+
+			{/* Total items count */}
+			<div className="mt-8 text-right">
+				<p className="text-2xl font-medium text-black">
+					Total Items:
+					<span className="font-bold ml-2">
+						{palletLabel.items.reduce(
+							(sum, item) => sum + Number(item.quantity), // should always be a number
+							0
+						)}
+					</span>
+				</p>
+			</div>
+
+			{/* Footer */}
+			<div className="mt-8 text-center text-sm text-gray-500">
+				https://pallet-labels.coreyh.nz
+			</div>
+		</div>
+	);
+};
+
+export default PrintPageContent;
